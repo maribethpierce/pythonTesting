03e152a7c2887fb4febdaa4908aad904addad85a
Maribeth Pierce <maribethpierce@gmail.com>
2018-08-15 12:42:37 -0400

    initial

---
+++ horoscope.py
@@ -0,0 +1,28 @@
+# file: horoscope.py
+def horoscope(month):
+    '''Return horoscope for given date.'''
+    if month == 1:
+        return 'Capricorn'
+    elif month == 2:
+         return 'Aquarius'
+    elif month == 3:
+         return 'Pisces'
+    elif month == 4:
+         return 'Aries'
+    elif month == 5:
+         return 'Taurus'
+    elif month == 6:
+         return 'Gemini'
+    elif month == 7:
+         return 'Cancer'
+    elif month == 8:
+         return 'Leo'
+    elif month == 9:
+         return 'Virgo'
+    elif month == 10:
+         return 'Libra'
+    elif month == 11:
+         return 'Scorpio'
+    elif month == 12:
+         return 'Sagittarius'
+
---
+++ test_horoscope.py
@@ -0,0 +1,15 @@
+# file: test_horoscope.py
+expect horoscope(january) == "Capricorn"
+expect horoscope(2018) == NIL
+expect horoscope(1) == "Capricorn"
+
+Homework:
+
+1. Read the following articles
+
+    * http://pythontesting.net/framework/unittest/unittest-introduction/
+    * http://pythontesting.net/framework/pytest/pytest-introduction/
+
+2. Send me some automated tests using pytest to exercise the horoscope method
+
+3. Send me an email answering: how do you handle learning new skills and give me a good example of how you overcame a situation where you had to learn something new?
